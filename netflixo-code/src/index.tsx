b3221642cbd04217e6a983865a27be1d91d5617c
Amanda <amanda.anjos@grupotecnoset.com.br>
2024-03-20 12:21:43 -0300

    inclusão do tailwind

--- netflixo-code/src/index.tsx
+++ netflixo-code/src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'; // Importe o arquivo CSS aqui
+import './main.css'; // Importe o arquivo CSS aqui
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <h1 className="text-3xl font-bold underline text-green-600">
+      Hello world!
+    </h1>
   </React.StrictMode>,
   document.getElementById('root')
 );
